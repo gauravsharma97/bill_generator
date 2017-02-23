a77084aa5ac74cc4eeee1ed92f6994efc8b537e7
Gaurav Sharma <gauravbhilwara@gmail.com>
2017-02-23 16:59:05 +0530

    Add files via upload

---
+++ src/Billling_textile.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package billling_textile;
+
+
+/**
+ *
+ * @author GAURAV-PC
+ */
+public class Billling_textile {
+   public static int a=0;
+    /**
+     * @param args the command line arguments
+     */
+   
+   
+   
+   
+   
+    public static void main(String[] args) {
+       new bill().setVisible(true);
+    }
+    
+}
